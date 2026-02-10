4f3f860deb45bdf7afd44a53f26af59036406c86
LeadsBilalRasool <f221663@leads.edu.pk>
2026-02-10 21:48:31 +0500

    add fronten cors env

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const cors = require('cors');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
